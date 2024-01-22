47ca743114f8afd9dc7890090d3588941525b98c
Jorge Villafraz <jorge9372@gmail.com>
2024-01-21 20:49:40 -0400

    Pruebas en componente GifIten

--- tests/components/GifIten.test.jsx
+++ tests/components/GifIten.test.jsx
@@ -1,11 +1,29 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { GifItem } from '../../src/components';
 
 describe('Pruebas en <GetItem />', () => {
     const title = 'Dragon Ball';
-    const url = 'https://api.giphy.com';
+    const url = 'https://api.giphy.com/dragonball.jpg';
     test('Debe hacer match con el snapshot', () => {
         const { container } = render(<GifItem title={title} url={url} />);
         expect(container).toMatchSnapshot();
+    });
+
+    test('debde de mostrar la imagen con el url y el ALT indicado', () => {
+        render(<GifItem title={title} url={url} />);
+        // screen.debug();
+
+        // expect(screen.getByRole('img').src).toBe(url);
+        // expect(screen.getByRole('img').alt).toBe(title);
+
+        const { src, alt } = screen.getByRole('img');
+        expect(src).toBe(url);
+        expect(alt).toBe(title);
+    });
+
+    test('debe mostrar el titulo en el compoente', () => {
+        render(<GifItem title={title} url={url} />);
+
+        expect(screen.getByText(title)).toBeTruthy();
     });
 });
